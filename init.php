3f794580433828b0a171945d29e1f0d3afcc6c4f
loosq <ipsa87@gmail.com>
2019-05-30 19:51:47 +0300

    для коммита

--- init.php
+++ init.php
@@ -9,8 +9,8 @@ setlocale(LC_ALL, 'ru_RU');
 
 //Подключение внешних файлов
 include_once 'vendor/autoload.php';
-include_once 'functions/helpers.php';
 include_once 'functions/notify.php';
+include_once 'functions/helpers.php';
 include_once 'functions/database.php';
 include_once 'functions/routines.php';
 include_once 'functions/formValidation.php';
